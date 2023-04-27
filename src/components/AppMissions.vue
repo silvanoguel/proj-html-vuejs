5383dac8fdff4522c044e01c0c7a18009eef6dca
silvanoguel <silvanoguel@msn.com>
2023-04-27 10:32:33 +0200

    add: mission section

--- src/components/AppMissions.vue
+++ src/components/AppMissions.vue
@@ -9,7 +9,22 @@ export default {
 
     <div class="missions">
 
-    <img class="faded-image"  alt="">
+        <img class="faded-image"  alt="">
+        <img class="woman-image" src="../assets/img/businesswoman-analysing-document-P8WSNMC-1024x820.jpg" alt="">
+
+        <div class="mission-text">
+            <div class="mission-title">
+                <p>learn more about our <span>missions</span></p>
+            </div>
+            <div class="lines d-flex my-4">
+                <hr class="small-line me-1">
+                <hr class="big-line">
+            </div>
+            <div class="mission-paragraph">
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large languace ocean. Separated they live in Bookmarksgrove.</p>
+            </div>
+            <button class="green-button me-4">VIEW ALL</button>
+        </div>
 
     </div>
 
@@ -21,9 +36,10 @@ export default {
 @use '../styles/partials/variables' as *;
 
 .missions {
-    background-color: grey;
+    background-color:#F9F9F9;
     padding-top: 6rem;
     padding-bottom: 10rem;
+    position: relative;
 }
 .faded-image {
     background-image:
@@ -33,7 +49,58 @@ export default {
     height: 400px;
     background-repeat: no-repeat;
     background-size: cover;
+    border-top-right-radius: 20px;
+    border: none;
 }
 
+.woman-image {
+    width: 30%;
+    position: absolute;
+    top: 25%;
+    left: 13%;
+    border-radius: 20px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
 
+.mission-text {
+    position:absolute;
+    top: 25%;
+    right: 20%;
+    width: 20%
+}
+
+span {
+    color: $secondary-text-color;
+}
+.mission-title {
+    font-size: 3rem;
+    line-height: 3.2rem;
+}
+.small-line {
+    background-image: $green-gradient; 
+    height: 5px; 
+    border: 0;
+    border-radius: 5px;
+    width: 3%;
+    opacity: 1;
+}
+
+.big-line {
+    background-image: $green-gradient; 
+    height: 5px; 
+    border: 0;
+    border-radius: 5px;
+    width: 10%;
+    opacity: 1;
+}
+.green-button {
+    background: $green-gradient;
+    color: white;
+    border-radius: 30px;
+    border: none;
+    padding: 0.8rem 2rem;
+}
+.mission-paragraph {
+    font-size: 0.7rem;
+}
 </style>
